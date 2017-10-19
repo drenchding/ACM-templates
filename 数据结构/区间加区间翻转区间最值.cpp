c99a7de465a232ba537ee12296cb623b18d3f90e
drenchding <847426363@qq.com>
2017-10-20 01:12:50 +0800

    update

--- 数据结构/区间加区间翻转区间最值.cpp
+++ 数据结构/区间加区间翻转区间最值.cpp
@@ -6,118 +6,125 @@
 //2 l r 翻转[l,r]
 //3 l r 询问[l,r]最大值
 //初始时序列都为0
+
 #include <cstdio>
 #include <iostream>
+
 const int MAXN = 200005;
+
 using namespace std;
-int n, m, cnt, opt, l, r;
+
+int n, m, segNode, opt, l, r;
+
 struct Node {
-    int size, value, add, rev;
-    Node *ls, *rs;
-    Node(){ ls = rs = NULL; }
+  int size, val, add, rev;
+  Node *ls, *rs;
+  Node(): add(0), rev(0), ls(NULL), rs(NULL) { }
+  Node(int s, int v, Node *l, Node *r): size(s), val(v), ls(l), rs(r), add(0), rev(0) { }
 }pool[MAXN], *st[MAXN], *root, *null;
-inline int read() {
-    int x = 0, f = 1; char ch = getchar();
-    while(!isdigit(ch)) { 
-        if(ch == '-') f = -1;
-        ch = getchar();
-    }
-    while(isdigit(ch)) { 
-        x = x * 10 + ch - '0';
-        ch = getchar(); 
-    }
-    return x * f;
+
+Node *newNode(int s, int v, Node *l, Node *r) {
+  return &(*st[segNode++] = Node(s, v, l, r));
 }
-inline Node *newnode() {
-    return &*st[cnt++];
+
+int read() {
+  int x; scanf("%d", &x); return x;
 }
-inline void pushdown(Node *cur) {
-    if(cur->add) {
-        if(!cur->ls->size) {
-            cur->value += cur->add;
-        } else {
-            cur->ls->add += cur->add;
-            cur->rs->add += cur->add;
-        }
-        cur->add = 0;
+
+void pushdown(Node *cur) {
+  if(cur->add) {
+    if(!cur->ls->size) {
+      cur->val += cur->add;
+    } else {
+      cur->ls->add += cur->add;
+      cur->rs->add += cur->add;
     }
-    if(cur->rev) {
-        if(cur->ls->size) {
-            cur->ls->rev ^= 1;
-            cur->rs->rev ^= 1;
-            swap(cur->ls, cur->rs);
-        }
-        cur->rev = 0;
-    }
-}
-inline void update(Node *cur) {
+    cur->add = 0;
+  }
+  if(cur->rev) {
     if(cur->ls->size) {
-        cur->size = cur->ls->size + cur->rs->size;
-        cur->value = max(cur->ls->value + cur->ls->add, cur->rs->value + cur->rs->add);
+      cur->ls->rev ^= 1;
+      cur->rs->rev ^= 1;
+      swap(cur->ls, cur->rs);
     }
+    cur->rev = 0;
+  }
 }
-inline Node *merge(Node *a, Node *b) {
-    pushdown(a); pushdown(b);
-    update(a); update(b);
-    Node *cur = newnode();
-    cur->size = a->size + b->size;
-    cur->value = max(a->value, b->value);
-    cur->ls = a; cur->rs = b;
-    return cur;
+
+void pushup(Node *cur) {
+  if(cur->ls->size) {
+    cur->size = cur->ls->size + cur->rs->size;
+    cur->val = max(cur->ls->val + cur->ls->add, cur->rs->val + cur->rs->add);
+  }
 }
+
+Node *merge(Node *a, Node *b) {
+  pushdown(a); pushup(a); 
+  pushdown(b); pushup(b);
+  if(a->size > b->size * 4) {
+    a->rs = merge(a->rs, b);
+    pushup(a); return a;
+  }
+  if(b->size > a->size * 4) {
+    b->ls = merge(a, b->ls);
+    pushup(b); return b;
+  }
+  Node *cur = newNode(a->size + b->size, max(a->val, b->val), a, b);
+  return cur;
+}
+
 void split(Node *cur, int x) {
-    pushdown(cur);
-    if(x > cur->ls->size) {
-        split(cur->rs, x - cur->ls->size);
-        cur->ls = merge(cur->ls, cur->rs->ls);
-        st[--cnt] = cur->rs; 
-        cur->rs = cur->rs->rs;
-    } else if(x < cur->ls->size) {
-        split(cur->ls, x);
-        cur->rs = merge(cur->ls->rs, cur->rs);
-        st[--cnt] = cur->ls;
-        cur->ls = cur->ls->ls;
-    }
-    update(cur);
+  pushdown(cur);
+  if(x > cur->ls->size) {
+    split(cur->rs, x - cur->ls->size);
+    cur->ls = merge(cur->ls, cur->rs->ls);
+    st[--segNode] = cur->rs; 
+    cur->rs = cur->rs->rs;
+  } else if(x < cur->ls->size) {
+    split(cur->ls, x);
+    cur->rs = merge(cur->ls->rs, cur->rs);
+    st[--segNode] = cur->ls;
+    cur->ls = cur->ls->ls;
+  }
+  pushup(cur);
 }
-inline Node *getinterval(int l, int r) {
-    split(root, r);
-    split(root->ls, l - 1);
-    return root->ls->rs;
+
+Node *getinterval(int l, int r) {
+  split(root, r);
+  split(root->ls, l - 1);
+  return root->ls->rs;
 }
+
 Node *build(int l, int r) {
-    if(l == r) {
-        Node *cur = newnode();
-        cur->size = 1;
-        cur->value = 0;
-        cur->ls = null;
-        cur->rs = null;
-        return cur;
-    }
-    int mid = (l + r) / 2;
-    Node *ls = build(l, mid);
-    Node *rs = build(mid + 1, r);
-    return merge(ls, rs);
+  if(l == r) {
+    Node *cur = newNode(1, 0, null, null);
+    return cur;
+  }
+  int mid = (l + r) / 2;
+  Node *ls = build(l, mid);
+  Node *rs = build(mid + 1, r);
+  return merge(ls, rs);
 } 
+
 int main() {
-    n = read(); m = read();
-    for(int i = 0; i < MAXN; i++) {
-        st[i] = &pool[i];
+  n = read(); m = read();
+  for(int i = 0; i < MAXN; i++) {
+    st[i] = &pool[i];
+  }
+  null = newNode(0, 0, 0, 0);
+  root = build(0, n + 1);
+  while(m--) {
+    opt = read(); l = read() + 1; r = read() + 1;
+    Node *cur = getinterval(l, r);
+    if(opt == 1) {
+      cur->add += read();
+    } else if(opt == 2) {
+      cur->rev ^= 1;
+    } else {
+      pushdown(cur);
+      pushup(cur);
+      printf("%d\n", cur->val);
     }
-    null = newnode(); null->size = 0;
-    root = build(0, n + 1);
-    while(m--) {
-        opt = read(); l = read() + 1; r = read() + 1;
-        Node *cur = getinterval(l, r);
-        if(opt == 1) {
-            cur->add += read();
-        } else if(opt == 2) {
-            cur->rev ^= 1;
-        } else {
-            pushdown(cur);
-            update(cur);
-            printf("%d\n", cur->value);
-        }
-    }
-    return 0;
+  }
+  return 0;
 }
--- 数论/Lucas定理求组合数.cpp
+++ 数论/Lucas定理求组合数.cpp
@@ -3,25 +3,42 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-const int MOD = 1000003;
+
+const int MAXMOD = 100005;
+
 typedef long long ll;
-ll fac[MOD], inv[MOD];
+
+ll fac[MAXMOD], inv[MAXMOD];
+int MOD;
+
 void init() {
-    int i;
-    for(fac[0] = 1, i = 1; i < MOD; i++) {
-        fac[i] = fac[i-1] * i % MOD;
-    }
-    for(inv[1] = 1, i = 2; i < MOD; i++) {
-        inv[i] = (MOD - MOD / i) * inv[MOD % i] % MOD;
-    }
-    for(inv[0] = 1, i = 1; i < MOD; i++) {
-        (inv[i] *= inv[i-1]) %= MOD;
-    }
+  int i;
+  for(fac[0] = 1, i = 1; i < MOD; i++) {
+    fac[i] = fac[i - 1] * i % MOD;
+  }
+  for(inv[1] = 1, i = 2; i < MOD; i++) {
+    inv[i] = (MOD - MOD / i) * inv[MOD % i] % MOD;
+  }
+  for(inv[0] = 1, i = 1; i < MOD; i++) {
+    (inv[i] *= inv[i - 1]) %= MOD;
+  }
 }
+
 ll C(int n, int m) {
-    if(n < m) return 0;
-    if(n < MOD && m < MOD) {
-        return fac[n] * inv[m] % MOD * inv[n - m] % MOD;
-    }
-    return C(n / MOD, m / MOD) * C(n % MOD, m % MOD) % MOD;
+  if(n < m) return 0;
+  if(n < MOD && m < MOD) {
+    return fac[n] * inv[m] % MOD * inv[n - m] % MOD;
+  }
+  return C(n / MOD, m / MOD) * C(n % MOD, m % MOD) % MOD;
+}
+
+int main() {
+  int T; scanf("%d", &T);
+  while(T--) {
+    int n, m; 
+    scanf("%d %d %d", &n, &m, &MOD);
+    init();
+    printf("%lld\n", C(n + m, m));
+  }
+  return 0;
 }
