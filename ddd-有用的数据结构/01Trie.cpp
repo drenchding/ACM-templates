c776ca7eb92e28d2f960568351f11ef68d0de613
drenchding <847426363@qq.com>
2018-02-02 22:20:50 +0800

    upd
    upd

--- ddd-有用的数据结构/01Trie.cpp
+++ ddd-有用的数据结构/01Trie.cpp
@@ -1,20 +1,24 @@
 namespace Trie {
   const int LEN = 31;
+  struct Node *null;
   struct Node {
     int val;
     Node *ch[2];
     void init() {
       val = 0;
-      ch[0] = ch[1] = NULL;
+      ch[0] = ch[1] = null;
     }
   }pool[MAXN * LEN], *root;
   int d[LEN], res, segNode;
-  void clear() { 
-    segNode = 0, root = NULL; 
-  }
   Node *newNode() {
     pool[segNode].init();
     return &pool[segNode++];
+  }
+  void clear() { 
+    segNode = 0;  
+    null = newNode();
+    null->ch[0] = NULL;
+    root = null;
   }
   void pre(int val) {
     for(int i = 1; i <= LEN; i++) {
@@ -23,21 +27,18 @@ namespace Trie {
     }
   }
   void add(Node *&cur, int deep, int v) {
-    if(cur == NULL) cur = newNode();
+    if(!cur->ch[0]) cur = newNode();
     cur->val += v;
     if(deep == LEN) return;
     add(cur->ch[d[deep]], deep + 1, v);
   }
-  inline bool ok(Node *cur) {
-    return cur && cur->val;
-  }
   void maxXor(Node *cur, int deep) {
     if(deep == LEN) return;
     int bit = d[deep];
-    if(ok(cur->ch[bit ^ 1])) {
+    if(cur->ch[bit ^ 1]->val) {
       res += (1 << (LEN - deep - 1));
       maxXor(cur->ch[bit ^ 1], deep + 1);
-    } else if(ok(cur->ch[bit])) {
+    } else if(cur->ch[bit]->val) {
       maxXor(cur->ch[bit], deep + 1);
     }
   }
