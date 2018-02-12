d4536cc932c85b96be6c292b2539ab23cfd058cb
drenchding <847426363@qq.com>
2018-02-12 10:23:34 +0800

    upd

--- ddd-多项式/多项式大全.cpp
+++ ddd-多项式/多项式大全.cpp
@@ -1,10 +1,13 @@
 #include <cmath>
 #include <vector>
 #include <cstdio>
+#include <cstring>
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
+
+typedef long long ll;
 
 namespace FFT {
   const double PI = acos(-1);
@@ -44,7 +47,7 @@ namespace FFT {
       }
     }
   }
- 
+
   void conv(Poly & x, Poly & y, Poly & z) {
     if(x.size() == 1 && y.size() == 1) {
       z.resize(1); z[0] = x[0] * y[0]; return;
@@ -80,9 +83,95 @@ namespace FFT {
     delete[] w; delete[] a; delete[] b; delete[] c;
   }
 }
+
+namespace MTT {
+  const double PI = acos(-1);
+  const int MOD = 1e9 + 7;
+  
+  typedef vector<int> Poly;
+   
+  struct Comp {
+    double x, y;
+    Comp(double X = 0, double Y = 0): x(X), y(Y) { }
+    friend Comp operator+(const Comp & a, const Comp & b) {
+      return Comp(a.x + b.x, a.y + b.y);
+    }
+    friend Comp operator-(const Comp & a, const Comp & b) {
+      return Comp(a.x - b.x, a.y - b.y);
+    }
+    friend Comp operator*(const Comp & a, const Comp & b) {
+      return Comp(a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x);
+    }
+    friend Comp operator!(const Comp & a) {
+      return Comp(a.x, -a.y);
+    }
+  }*w;
+
+  void dft(Comp *a, int n) {
+    for(int i = 0, j = 0; i < n; i++) {
+      if(i > j) swap(a[i], a[j]);
+      for(int k = n >> 1; (j ^= k) < k; k >>= 1);
+    }
+    for(int i = 2, b = n >> 1; i <= n; i <<= 1, b >>= 1) {
+      for(int j = 0; j < n; j += i) {
+        Comp *l = a + j, *r = a + j + (i >> 1), *p = w;
+        for(int k = 0; k < (i >> 1); k++) {
+          Comp t = *r * *p;
+          *r = *l - t, *l = *l + t;
+          l++, r++, p += b;
+        }
+      }
+    }
+  }
+
+  void conv(Poly & x, Poly & y, Poly & z) {
+    int t = 0, n = x.size() + y.size() - 1;
+    while((1 << t) < n) t++;
+    n = (1 << t);
+    x.resize(n); y.resize(n);
+    w = new Comp[n]; 
+    Comp *a = new Comp[n], *b = new Comp[n];
+    for(int i = 0; i < n; i++) {
+      w[i] = Comp(cos(2 * PI * i / n), sin(2 * PI * i / n));
+      (x[i] += MOD) %= MOD; (y[i] += MOD) %= MOD;
+      a[i] = Comp(x[i] & 32767, x[i] >> 15);
+      b[i] = Comp(y[i] & 32767, y[i] >> 15);
+    }
+    dft(a, n); dft(b, n);
+    Comp *dfta = new Comp[n], *dftb = new Comp[n];
+    Comp *dftc = new Comp[n], *dftd = new Comp[n];
+    for(int i = 0; i < n; i++) {
+      int j = (n - i) & (n - 1);
+      static Comp da, db, dc, dd;
+      da = (a[i] + !a[j]) * Comp(0.5, 0);
+      db = (a[i] - !a[j]) * Comp(0, -0.5);
+      dc = (b[i] + !b[j]) * Comp(0.5, 0);
+      dd = (b[i] - !b[j]) * Comp(0, -0.5);
+      dfta[j] = da * dc;
+      dftb[j] = da * dd;
+      dftc[j] = db * dc;
+      dftd[j] = db * dd;
+    }
+    for(int i = 0; i < n; i++) {
+      a[i] = dfta[i] + dftb[i] * Comp(0, 1);
+      b[i] = dftc[i] + dftd[i] * Comp(0, 1);
+    }
+    dft(a, n); dft(b, n);
+    z.resize(n);
+    for(int i = 0; i < n; i++) {
+      int da = (ll)(a[i].x / n + 0.5) % MOD;
+      int db = (ll)(a[i].y / n + 0.5) % MOD;
+      int dc = (ll)(b[i].x / n + 0.5) % MOD;
+      int dd = (ll)(b[i].y / n + 0.5) % MOD;
+      z[i] = (da + ((ll)(db + dc) << 15) + ((ll)dd << 30)) % MOD;
+    }
+    delete[] w; delete[] a; delete[] b;
+    delete[] dfta; delete[] dftb; 
+    delete[] dftc; delete[] dftd;
+  }
+}
  
 namespace NTT {
-  typedef long long ll;
   typedef vector<int> Poly;
  
   /*
