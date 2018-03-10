45f055458e75dbdf6fc1c1c241d5210f82a11444
drenchding <847426363@qq.com>
2018-03-10 13:51:57 +0800

    upd

--- ddd-图论/最短路.cpp
+++ ddd-图论/最短路.cpp
@@ -1,36 +1,37 @@
-void spfa(int sta) {
-    memset(dis, 127, sizeof(dis));
-    dis[sta] = 0; 
-    queue<int>Q; Q.push(sta);
-    while(!Q.empty()) {
-        int cur = Q.front(); Q.pop();
-        for(int i = 0; i < G[cur].size(); i++) {
-            int nx = G[cur][i].to;
-            int newd = G[cur][i].dis + dis[cur];
-            if(newd < dis[nx]) {
-                dis[nx] = newd;
-                pre[nx] = cur;
-                Q.push(nx);
-            }
-        }
+#include <queue>
+#include <cstdio>
+#include <vector>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+namespace Dis {
+    const int MAXN = 500005;
+    int dis[MAXN], vis[MAXN], pre[MAXN];
+    typedef pair<int, int> PII;
+    vector<PII> G[MAXN];
+    void addEdge(int x, int y, int z) {
+        G[x].push_back(make_pair(y, z));
     }
-}
-void dijkstra(int sta) {
-    #define pr pair<int, int>
-    priority_queue<pr, vector<pr>, greater<pr> >Q;
-    memset(dis, 127, sizeof(dis));
-    dis[sta] = 0;
-    Q.push(make_pair(0, sta));
-    while(!Q.empty()) {
-        int cur = Q.top().second; Q.pop();
-        if(vis[cur]) continue;
-        vis[cur] = 1;
-        for(int i = 0; i < G[cur].size(); i++) {
-            int nx = G[cur][i].to;
-            if(dis[cur] + G[cur][i].dis < dis[nx]) {
-                dis[nx] = dis[cur] + G[cur][i].dis;
-                pre[nx] = cur;
-                Q.push(make_pair(dis[nx], nx));
+    void dijkstra(int s) {
+        priority_queue<PII, vector<PII>, greater<PII> > Q;
+        memset(dis, 127, sizeof(dis));
+        dis[s] = 0;
+        Q.push(make_pair(0, s));
+        while(!Q.empty()) {
+            int cur = Q.top().second; Q.pop();
+            if(!vis[cur]) {
+                vis[cur] = 1;
+                for(int i = 0; i < G[cur].size(); i++) {
+                    int nx = G[cur][i].first;
+                    int d = G[cur][i].second;
+                    if(dis[cur] + d < dis[nx]) {
+                        dis[nx] = dis[cur] + d;
+                        pre[nx] = cur;
+                        Q.push(make_pair(dis[nx], nx));
+                    }
+                }
             }
         }
     }
