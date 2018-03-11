e6278a3a83f7c071050144f264f63d4072686f8d
drenchding <847426363@qq.com>
2018-03-11 23:25:33 +0800

    3.11

---
+++ ddd-图论/Hungarian二分图最大匹配.cpp
@@ -0,0 +1,35 @@
+namespace Hungarian {
+  const int MAXM = 250005;
+  struct Edge {
+    int v, n;
+  }e[MAXM];
+  int s[MAXM], mat[MAXM], vis[MAXM];
+  int tot, tm, ans;
+  void clear() {
+    memset(s, 0, sizeof(s));
+    memset(mat, 0, sizeof(mat));
+    memset(vis, 0, sizeof(vis));
+    tot = tm = ans = 0;
+  }
+  inline void addEdge(int u, int v) {
+    e[++tot] = (Edge){v, s[u]};
+    s[u] = tot;
+  }
+  bool dfs(int u) {
+    for(int i = s[u]; i; i = e[i].n) if(vis[e[i].v] < tm) {
+      vis[e[i].v] = tm;
+      if((!mat[e[i].v]) || dfs(mat[e[i].v])) {
+        mat[e[i].v] = u; return 1;
+      }
+    }
+    return 0;
+  }
+  inline void find(int st) {
+    tm++; if(dfs(st)) ans++;
+  }
+  void solve(int n, int m) { // left: [1, n]，right: [1, m]，Edge: left -> right
+    for(int i = 1; i <= n; i++) find(i);
+    printf("%d\n", ans);
+    for(int i = 1; i <= m; i++) printf("%d ", mat[i]);; // mat[i] 表示右边第 i 个人和左边哪个人匹配
+  }
+}
--- ddd-字符串/hash.cpp
+++ ddd-字符串/hash.cpp
@@ -1,23 +1,25 @@
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-using namespace std;
-typedef unsigned long long ull;
-const ull base = 2333;
-const int MAXN = 100005;
-char s[MAXN];
-ull p[MAXN], h[MAXN];
-ull gethash(int l, int r) {
-    return h[r] - h[l - 1] * p[r - l + 1];
-}
-void init() {
-    p[0] = 1;
-    int n = strlen(s + 1);
+namespace Hash {
+  typedef long long ll;
+  const ll base1 = 131;
+  const ll base2 = 2333;
+  const ll MOD1 = 1e9 + 7;
+  const ll MOD2 = 1e9 + 9;
+  const int MAXN = 100005;
+  ll p1[MAXN], p2[MAXN], h1[MAXN], h2[MAXN];
+  void init(char s[], int n) { // 1-based
+    p1[0] = p2[0] = 1;
+    h1[0] = h2[0] = 0;
     for(int i = 1; i <= n; i++) {
-        p[i] = p[i - 1] * base;
-        h[i] = h[i - 1] * base + s[i];
+      p1[i] = (p1[i - 1] * base1) % MOD1;
+      p2[i] = (p2[i - 1] * base2) % MOD2;
+      h1[i] = (h1[i - 1] * base1 + s[i]) % MOD1;
+      h2[i] = (h2[i - 1] * base2 + s[i]) % MOD2;
     }
+  }
+  pair<ll, ll> getHash(int l, int r) {
+    ll H1 = ((h1[r] - h1[l - 1] * p1[r - l + 1]) % MOD1 + MOD1) % MOD1;
+    ll H2 = ((h2[r] - h2[l - 1] * p2[r - l + 1]) % MOD2 + MOD2) % MOD2;
+    return make_pair(H1, H2);
+  }
 }
-int main() {
-    return 0;
-}
+
