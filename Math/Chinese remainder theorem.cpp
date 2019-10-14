69fa64fc0ed8a82aed316997c5723a1c67f7ccf9
Cong <847426363@qq.com>
2019-10-14 22:31:24 +0800

    Fix format

--- Math/Chinese remainder theorem.cpp
+++ Math/Chinese remainder theorem.cpp
@@ -1,11 +1,11 @@
 // 初始在模 MOD 意义下为 x，新加一个 ax = b (mod m)，更新 x, MOD
 // 初始时条件可以设 x = 0, MOD = 1，之后不断添加条件
 bool chineseRemainder(ll &x, ll &MOD, int a, int b, int m) {
-	ll p, q;
-	int g = exgcd(a * MOD, m, p, q), t = m / g;
-	ll c = b - a * x;
-	if(c % g) return false;
-	p = ((p + m) * (c / g % t + t)) % t;
-	x += p * MOD; MOD *= t;
-	return true;
+  ll p, q;
+  int g = exgcd(a * MOD, m, p, q), t = m / g;
+  ll c = b - a * x;
+  if(c % g) return false;
+  p = ((p + m) * (c / g % t + t)) % t;
+  x += p * MOD; MOD *= t;
+  return true;
 }
