238239b0aef545a7465bd7d8c5f5cf3bacf05be9
justarandomstring <847426363@qq.com>
2021-05-24 10:47:34 +0800

    Update Balanced binary tree.cpp

--- Data structure/Balanced binary tree.cpp
+++ Data structure/Balanced binary tree.cpp
@@ -7,25 +7,24 @@ struct BBT {
   typedef int DATA;
   typedef tree<pair<DATA, int>, null_type, less<pair<DATA, int> >, rb_tree_tag, 
           tree_order_statistics_node_update> BST;
-  // 如果 CE，把 null_type 换成 null_mapped_type
-  map<DATA, int> M;
   BST B;
   size_t size() {
     return B.size();
   }
   void insert(int x) {
-    M[x]++, B.insert(make_pair(x, M[x]));
+    static int count = 0;
+    B.insert(make_pair(x, ++count));
   }
   void erase(int x) {
-    if(M[x]) B.erase(make_pair(x, M[x])), M[x]--;
+    B.erase(B.lower_bound(make_pair(x, -1)));
   }
   int query(int x) { // 多少个 <=x 的数
     return B.order_of_key(make_pair(x + 1, -1));
   }
   int kth(int k) { // 查询排名为 k 的数 (1-based)
-    return (*B.find_by_order(k - 1)).first;
+    return B.find_by_order(k - 1)->first;
   }
   void clear() {
-    M.clear(); B.clear();
+    B.clear();
   }
-}B;
+};
